d6f364b962f5cf6a47d6f126d0452dde0869b190
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-06-21 22:16:59 +0400

    modeify urls blackjack

--- Games/views.py
+++ Games/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Games import Game_Class
 
 
 def blackjack_game(request):
-    return render(request, 'BLJC.html')
+    # context  = 
+        
+    return render(request, 'BLJC.html' , context)
