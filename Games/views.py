a64e26c60777015fd63c098a19b3debdd5c850d0
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-06-22 23:38:04 +0400

    p

--- Games/views.py
+++ Games/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from Games import Game_Class
 
 
-def blackjack_game(request):
-    # context  = 
-        
-    return render(request, 'BLJC.html' , context)
+
+def blackjack_game(request):     
+    return render(request, 'bj.html')
