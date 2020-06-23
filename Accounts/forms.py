e341f2fa8914001d456df89e4a591c64d4d4b961
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-06-24 00:07:39 +0400

    update form

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -4,12 +4,34 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 
+STATES = (
+    ('', 'Choose...'),
+    ('MG', 'Minas Gerais'),
+    ('SP', 'Sao Paulo'),
+    ('RJ', 'Rio de Janeiro')
+)
+
 
 class RegisterForm(UserCreationForm):
 
-    email = forms.EmailField()
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput())
+    address_1 = forms.CharField(
+        label='Address',
+        widget=forms.TextInput(attrs={'placeholder': '1234 Main St'})
+    )
+    address_2 = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Apartment, studio, or floor'})
+    )
+    city = forms.CharField()
+    state = forms.ChoiceField(choices=STATES)
+    zip_code = forms.CharField(label='Zip')
+    check_me_out = forms.BooleanField(required=False)
 
-    class Meta:
-        model = User
-        fields = ["username", "email",
-                  "password1", "password2"]
+
+class Meta:
+    model = User
+    fields = ["username", "email",
+              "password1", "password2"]
