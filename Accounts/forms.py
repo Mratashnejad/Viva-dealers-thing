d8972da7e4b5a26186e2d78634e250b664702cc4
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-06-24 00:16:54 +0400

    crispy formHelper

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 STATES = (
     ('', 'Choose...'),
@@ -29,6 +31,28 @@ class RegisterForm(UserCreationForm):
     state = forms.ChoiceField(choices=STATES)
     zip_code = forms.CharField(label='Zip')
     check_me_out = forms.BooleanField(required=False)
+
+
+def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.helper = FormHelper()
+    self.helper.layout = Layout(
+        Row(
+            Column('email', css_class='form-group col-md-6 mb-0'),
+            Column('password', css_class='form-group col-md-6 mb-0'),
+            css_class='form-row'
+        ),
+        'address_1',
+        'address_2',
+        Row(
+            Column('city', css_class='form-group col-md-6 mb-0'),
+            Column('state', css_class='form-group col-md-4 mb-0'),
+            Column('zip_code', css_class='form-group col-md-2 mb-0'),
+            css_class='form-row'
+        ),
+        'check_me_out',
+        Submit('submit', 'Sign in')
+    )
 
 
 class Meta:
