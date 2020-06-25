3fb4dfb455eda6b5788222e13bbb82cfe92eb5ba
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-06-25 10:01:08 +0400

    login required to access courses data

--- Courses/views.py
+++ Courses/views.py
@@ -1,30 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def Courses_page(request):
     return render(request, 'index.html')
 
+# login required to access the courses data
 
+
+@login_required(login_url="accounts/login")
 def Blackjack_page(request):
     return render(request, 'Blackjack.html')
 
 
+@login_required(login_url="accounts/login")
 def Poker_page(request):
     return render(request, 'Poker.html')
 
 
+@login_required(login_url="accounts/login")
 def Baccarat_page(request):
     return render(request, 'Baccarat.html')
 
 
+@login_required(login_url="accounts/login")
 def Keno_page(reqeust):
     return render(reqeust, 'Keno.html')
 
 
+@login_required(login_url="accounts/login")
 def Roulatte_page(reuqest):
     return render(reuqest, 'Roulatte.html')
 
 
+@login_required(login_url="accounts/login")
 def Dragontiger_page(reuqest):
     return render(reuqest, 'Dragontiger.html')
