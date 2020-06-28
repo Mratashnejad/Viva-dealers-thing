826f8966ae094ad9f346e8e9a1923e4ef6b4032d
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-06-28 12:22:15 +0400

    import to fields to admin page

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+User = get_user_model()
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+
+    class Meta:
+        model = User
+
+
+admin.site.register(User, UserAdmin)
