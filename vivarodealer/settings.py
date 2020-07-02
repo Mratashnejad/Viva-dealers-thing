1e36fd5fc3e9a3423febfc9d372bc9d914e738ea
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-07-02 05:31:25 +0400

    delete core temp

--- vivarodealer/settings.py
+++ vivarodealer/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Core',
+    
     'ecommerce',
     'crispy_forms',
     'Courses',
