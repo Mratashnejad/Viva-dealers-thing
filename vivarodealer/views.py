9f9f1bc75a37696de4d5adbe8d65a38b34dcae5b
Alireza Atashnejad <alirezaatashnejad@gmail.com>
2020-07-23 07:47:01 +0400

    go back

--- vivarodealer/views.py
+++ vivarodealer/views.py
@@ -1,21 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
-from ecommerce.models import Item,UserProfile
+from ecommerce.models import Item, UserProfile
 from django.views.generic import ListView, DetailView, View
+
 
 class homepageview(ListView):
     def get_context_data(self, **kwargs):
-        context['cat'] = ['cloths', 'bags', 'uniforms']
-        if 'bags' in request.POST:
-            context['items'] = Obj.objects.all.filter(name="bags")
+        context['cat'] = [('Cl', 'Clothes and Uniforms'),
+                          ('Ph', 'Phones & Accessories'),
+                          ('Je', 'Jewelry & Watches'),
+                          ('Ba', 'Bags & Shoes'),
+                          ('HA', 'Home Appliance'),
+                          ('Be', 'Beauty & Health, Hair'),
+                          ('Ap', 'Apartments'),
+                          ('Ho', 'Houses')]
+        if 'Cl' in request.POST:
+            context['items'] = Obj.objects.all.filter(name="Cl")
+        if 'Ph' in request.POST:
+            context['items'] = Obj.objects.all.filter(name="Ph")
+        return render(request, 'index.html', context)
+
 
 def home_page(request):
     context = {
         'items': Item.objects.all(),
     }
-    return render(request, 'index.html' , context )
-
+    return render(request, 'index.html', context)
 
 
 def courses_page(request):
@@ -33,6 +44,6 @@ def contact_page(request):
 def blog_page(request):
     return render(request, 'blog.html')
 
-def error404(request , exception):
-      return render(request, '404.html')
-     
+
+def error404(request, exception):
+    return render(request, '404.html')
